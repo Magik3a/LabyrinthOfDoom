8fbff511ff67d1f4b3bc44844ff0f2893867246e
Magik3a <svetlin.krastanov90@gmail.com>
2015-02-21 16:03:25 +0200

    Game

---
+++ LabyrinthOfDoom/Level1Matrix.cs
@@ -0,0 +1,37 @@
+﻿
+namespace LabyrinthOfDoom
+{
+    class Level1Matrix
+    {
+        public static char[,] GetMatrix()
+        {
+            char[,] matrix =  {
+                {'*','*', '*', '*', '*', '*', '*', '*', '*' ,'*'},
+                {' ',' ', ' ', ' ', '*', ' ', ' ', ' ', ' ','*'},
+                {'*','*', '*', ' ', '*', ' ', '*', '*', ' ','*'},
+                {'*',' ', ' ', ' ', ' ', ' ', ' ', '*', ' ','*'}, 
+                {'*',' ', '*', '*', '*', '*', '*', '*', ' ','*'}, 
+                {'*',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' '},
+                {'*','*', '*', '*', '*', '*', '*', '*', '*','*'},
+                };
+            return matrix;
+        }
+
+
+        public static void PrintMatrix()
+        {
+            char[,] matrix = Level1Matrix.GetMatrix();
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    System.Console.Write("{0, 2}", matrix[row, col]);
+                }
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+}
+
