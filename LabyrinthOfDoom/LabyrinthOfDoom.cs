cdce6f0ea06c2c447d0d3f07c2a03abaf16ee164
Magik3a <svetlin.krastanov90@gmail.com>
2015-02-21 16:03:59 +0200

    Game

---
+++ LabyrinthOfDoom/LabyrinthOfDoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabyrinthOfDoom;
+
+
+namespace LabyrinthOfDoom
+{
+    class LabyrinthOfDoom
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Labyrinth Of Doom";
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Svetlio E pi4");
+
+            Console.WriteLine("\n \n");
+            
+             Level1Matrix.PrintMatrix();
+        }
+
+        
+
+        
+    }
+}
+
+
