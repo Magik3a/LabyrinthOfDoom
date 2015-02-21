7d1d5394b59eb14b4543452015968ddfe77fe591
Magik3a <svetlin.krastanov90@gmail.com>
2015-02-21 18:19:53 +0200

    Game

---
+++ LabyrinthOfDoom/Level2Matrix.cs
@@ -0,0 +1,30 @@
+﻿
+namespace LabyrinthOfDoom
+{
+    class Level2Matrix
+    {
+        public static bool[][] GetMatrix()
+        {
+            bool[][] mazeLayout = 
+                {
+                    new[] {true, true, true, true, true, true, true, true},
+                    new[] {true, true, true, true, true, true, true, true},
+                    new[] {true, false, false, false, false, false, false, true},
+                    new[] {true, false, true, true, true, true, false, true},
+                    new[] {true, false, true, false, false, false, false, true},
+                    new[] {true, false, true, false, false, false, false, true},
+                    new[] {true, false, true, false, false, false, false, true},
+                    new[] {true, false, true, false, true, true, true, true},
+                    new[] {true, false, true, false, false, false, false, true},
+                    new[] {true, false, true, true, true, true, false, true},
+                    new[] {true, false, false, false, false, false, false, false},
+                    new[] {true, true, true, true, true, true, true, true}
+                };
+
+            return mazeLayout;
+        }
+
+    }
+
+}
+
